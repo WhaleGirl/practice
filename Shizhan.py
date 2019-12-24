c5491fa39fc17a5721d940a20f86fddfce6e050f
wangqian05 <wangqian05@fangdd.com>
2019-12-24 16:40:19 +0800

    Initial commit

---
+++ Shizhan.py
@@ -0,0 +1,61 @@
+#moc_tn = "百年孤独是什么样的一种孤独"
+#print(moc_tn)
+#a = input()
+#b = input()
+#if a>b:max=a
+#print(max)
+
+#num=5
+#if num == 5:
+#    print("ok")
+
+#a = 10
+#b=20
+#r=a if a>b else b
+#print(r)
+
+#str="百年孤独"
+#for s in str:
+#    print(s)
+
+
+# 九九乘法表
+#for i in range(1, 9):
+#    for j in range(1, 9):
+#        print(str(i) + "*"+ str(j)+"="+str(i*j) +"\t", end=' ')
+#    print('')
+
+#逢七拍腿
+# num=1
+# # total=0
+# # for num in range(100):
+# #     if num%7==0:
+# #         continue
+# #     else:
+# #         string=str(num)
+# #         if string.endswith('7'):
+# #             continue
+# #     total+=1
+# # print(total)
+
+#模拟10086
+# num = input()
+# while num!=0:
+#     if num == '1':
+#         print("你还剩余99元话费")
+#     elif num==2:
+#         print("你还剩余5G流量")
+#     elif num==3:
+#         print("操作无效")
+#     else:
+#         print("exit")
+#         break
+#     num=input()
+
+
+#索引的下标
+
+verse=["heheda","hsdkf","jeflelw"]
+print(verse[-1])
+print(verse[0:1])
+
